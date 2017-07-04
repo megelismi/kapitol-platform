00facca8fddf44c5261f3644fc79ec7b8081aa61
Steven Duval Ruilova <sduval215@gmail.com>
2017-07-04 12:42:09 -0700

    initial platform commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../stylesheets/main.css';
+
+class App extends Component{
+    render(){
+        return(
+            <h1>Kapitol Platform</h1>
+        )
+    }
+}
+
+export default App;
+
