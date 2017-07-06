e4f4b330b6944e37413835bc701da1b0fe06cfc9
Steven Duval Ruilova <sduval215@gmail.com>
2017-07-06 16:27:21 -0700

    home page design implemented

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import '../stylesheets/css/index.css';
 
+let kapitol_logo = require('../images/kapitol_logo.png');
+
 class App extends Component{
+
     render(){
         return(
-            <h1>Kapitol Platform</h1>
+            <div id='fp-container' className="page-mid">
+                <img src={kapitol_logo} alt=""/>
+                <div id="fp-search">
+                    <input type="text" placeholder="Enter a member name.." className="border-light-gray"/>
+                    <button className="no-border kapitolRed">
+                        <i className="fa fa-search" aria-hidden="true"></i>
+                    </button>
+                </div>
+                <div id="fp-selector" className="select-style">
+                    <select className="dropdown" name="drop-down">
+                        <option value="members">Members</option>
+                        <option value="legislation">Legislation</option>
+                    </select>
+                </div>
+            </div>
         )
     }
 }
