fe70c5f46822c1cb33549e8022c15bbbd3e6d866
Steven Duval Ruilova <sduval215@gmail.com>
2017-07-06 22:18:16 -0700

    initial member/legislation state functionality setup

--- src/components/app.js
+++ src/components/app.js
@@ -4,19 +4,53 @@ import '../stylesheets/css/index.css';
 let kapitol_logo = require('../images/kapitol_logo.png');
 
 class App extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            placeholderText: 'Enter a member name..',
+            searchType: 'member',
+            inputValue: ''
+        }
+        this.setPlaceholder = this.setPlaceholder.bind(this);
+        this.updateInputValue = this.updateInputValue.bind(this);
+    }
+
+    //FUNCTION TO BE FIRED ON SELECTOR CHANGE
+    //CHECKS TARGET VALUE AND SETS PLACEHOLDER AND SEARCH TYPE STATE
+    setPlaceholder(event){
+        var target = event.target.value;
+        if(target === 'members'){
+            this.setState({
+                placeholderText: 'Enter a member name..',
+                searchType: target
+            })
+        }else{
+            this.setState({
+                placeholderText: 'Enter a keyword..',
+                searchType: target
+            })
+        }
+    }
+
+    updateInputValue(event){
+        var target = event.target.value;
+        console.log(target);
+        this.setState({
+            inputValue: target
+        })
+    }
+
+
 
     render(){
         return(
             <div id='fp-container' className="page-mid">
                 <img src={kapitol_logo} alt=""/>
                 <div id="fp-search">
-                    <input type="text" placeholder="Enter a member name.." className="border-light-gray"/>
-                    <button className="no-border kapitolRed">
-                        <i className="fa fa-search" aria-hidden="true"></i>
-                    </button>
+                    <input onChange={this.updateInputValue} type="text" placeholder={this.state.placeholderText} className="border-light-gray"/>
                 </div>
                 <div id="fp-selector" className="select-style">
-                    <select className="dropdown" name="drop-down">
+                    <select onChange={this.setPlaceholder} className="dropdown" name="drop-down">
                         <option value="members">Members</option>
                         <option value="legislation">Legislation</option>
                     </select>
