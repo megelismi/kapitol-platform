348e7adb246c310aaab204ea449cd36a7cf57542
Steven Duval Ruilova <sduval215@gmail.com>
2017-07-19 00:56:42 -0700

    K-12 Code Refactoring

--- server/routes/members.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.get('/fetch-member-data', function(req,res){
-    res.json({success: false});
-})
-
-module.exports = router;
