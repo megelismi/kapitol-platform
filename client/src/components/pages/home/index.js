d8c5d5c8a6283be9ca77c81a8056a954bd9beaeb
Steven Duval Ruilova <sduval215@gmail.com>
2017-07-24 19:06:52 -0700

    K-15 New Loading UISnippet (Home Page)

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -2,6 +2,9 @@ import React,{Component} from 'react';
 import '../../../stylesheets/css/index.css';
 import SearchComponent from '../../shared/search';
 import FooterComponent from '../../shared/footer';
+import { connect } from 'react-redux';
+import { fetchList } from '../../../actions/getRequests';
+
 
 let kapitol_logo = require('../../../images/kapitol_logo.png');
 
@@ -9,12 +12,22 @@ let kapitol_logo = require('../../../images/kapitol_logo.png');
 class Home extends Component{
 
     componentDidMount(){
-        document.title = "Home | Kapitol"
+        document.title = "Home | Kapitol";
+        this.props.dispatch(fetchList());
     }
 
     render(){
         return(
             <div id='fp-container'>
+                {
+                    this.props.memberList.length > 0 ?
+                        <div></div> :
+                        <div className="loading">
+                            <div className="page-mid loading-screen">
+                                <span></span>
+                            </div>
+                        </div>
+                }
                 <div className="page-mid">
                     <img src={kapitol_logo} alt="Kapitol logo"/>
                     <div id="fp-search">
@@ -28,6 +41,11 @@ class Home extends Component{
     }
 }
 
+const mapStateToProps = state => ({
+    memberList: state.home.memberListReceived,
+    keywordList: state.home.keywordListReceived
+});
 
-export default Home;
+export default connect (mapStateToProps)(Home);
+
 
