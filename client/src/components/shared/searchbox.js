577704fd22b5cb8ad71e8f035ccb4c8cc1878af0
Steven Duval Ruilova <sduval215@gmail.com>
2017-07-30 23:24:14 -0700

    K-16 Query Bug Fix (#15)

--- client/src/components/shared/searchbox.js
+++ client/src/components/shared/searchbox.js
@@ -7,18 +7,19 @@ class SearchBox extends Component{
             <div id="search-box">
                 <ul id='search-options' className="no-padding no-margin text-left list-no-styles">
                     {this.props.searchType === 'members' ? this.props.searchResults.filter(word => {
-                        if(word.firstName.indexOf(this.props.value) > -1 ){
+                        if(word.firstName.indexOf(this.props.value) > -1 || word.lastName.indexOf(this.props.value) > -1 ){
                             return word;
                         }else{
                             return ''
                         }
                     }).map(word =>{
                         return (
-                            <Link key={word.memberId} to={`/${this.props.searchType}/${word.firstName}+${word.lastName}`}>
+                            <Link key={word.memberId} to={`/${this.props.searchType}/${word.firstName} ${word.lastName}`}>
                                 <li>
                                     <img className='memberPhoto' src={'https://theunitedstates.io/images/congress/225x275/' + word.memberId + '.jpg'}  alt=""/>
                                     <span>{word.firstName}</span>
-                                    <span>{word.lastName} </span></li>
+                                    <span>{word.lastName} </span>
+                                </li>
                             </Link>)
                     }) : this.props.searchResults.filter(word => {
                             if(word.includes(this.props.value)){
